bb8a755122f891acf95f21c41c3b3c0bd7a9b260
João <joaovitorresende.jr@gmail.com>
2026-08-27 19:42:58 -0300

    chore: adicionando perfil

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function toggleMode(){
+
     const html = document.documentElement
+
     html.classList.toggle('light')
+
     const img = document.querySelector("#profile img")
+
     if(html.classList.contains('light'))
     {
-        img.setAttribute("src","./avatar-light.png")
+        img.setAttribute("src","./photo.jpg")
     }
     else
     {
-        img.setAttribute("src","./avatar.png")
+        img.setAttribute("src","./photo.jpg")
     }
 }
